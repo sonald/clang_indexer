5b57fe4d03497d91273808dab112bfc53e80eeed
Sian Cao <sycao@redflag-linux.com>
2011-12-08 14:16:19 +0800

    fix find library for libclang

--- cmake/Modules/FindLibClang.cmake
+++ cmake/Modules/FindLibClang.cmake
@@ -1,6 +1,6 @@
 find_path(LibClang_INCLUDE_DIR clang-c/Index.h)
 
-find_library(LibClang_LIBRARY NAMES clang)
+find_library(LibClang_LIBRARY NAMES clang PATHS /usr/lib/llvm )
 
 set(LibClang_LIBRARIES ${LibClang_LIBRARY})
 set(LibClang_INCLUDE_DIRS ${LibClang_INCLUDE_DIR})
